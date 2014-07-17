1f2c9c2d61771f8e9ff0c623cfc067a15932bf02
miniminihouse <hayashi.minhee@O-04695-MAC.local>
2014-07-17 18:51:50 +0900

    Remove animation plugin

--- config.rb
+++ config.rb
@@ -1,6 +1,6 @@
 #require 'compass/import-once/activate'
 # Require any additional compass plugins here.
-require 'animation'
+# require 'animation'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
