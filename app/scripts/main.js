29276ddfb54d3e46dda6ee8e482dd43d42aa769c
Shindeok Kang <heavymery@gmail.com>
2014-07-19 14:48:59 +0900

    Add single page navigation

---
+++ app/scripts/main.js
@@ -0,0 +1,75 @@
+(function() {
+  
+  //------------------------------------------------------------------------------
+  //
+  //  Initialize
+  //
+  //------------------------------------------------------------------------------
+
+  var app = angular.module('miniminihouse', [
+    'ngRoute', 
+    'ngTouch', 
+    'ngAnimate',
+  ]);
+
+  //--------------------------------------
+  //  Routes
+  //--------------------------------------
+
+  app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    
+    $routeProvider.when('/', { templateUrl: 'views/top.html' });
+    $routeProvider.when('/profile', { templateUrl: 'views/profile.html' });
+    $routeProvider.when('/portfolio', { templateUrl: 'views/portfolio.html' });
+    $routeProvider.when('/contact', { templateUrl: 'views/contact.html' });
+    $routeProvider.otherwise('/');
+    
+    $locationProvider.html5Mode(true);
+    
+  }]);
+
+  //--------------------------------------
+  //  Controllers
+  //--------------------------------------
+
+  app.controller('MainCtrl', function($scope) {
+
+  });
+
+  //----------------------------------
+  //  RootScope
+  //----------------------------------
+
+  app.run(['$rootScope', '$location', function($rootScope, $location) {
+    
+    $rootScope.navigate = function(path, search) {
+      if(path) {
+        $location.path(path);
+      }
+
+      if(search) {
+        $location.search(search);
+      }
+    };
+    
+  }]);
+
+  angular.bootstrap(document, ['miniminihouse']);
+  
+  //------------------------------------------------------------------------------
+  //
+  //  Workarounds
+  //
+  //------------------------------------------------------------------------------
+  
+  //--------------------------------------
+  //  iOS Safari bug
+  //--------------------------------------
+  
+  window.addEventListener('orientationchange', function() {
+    if(90 === window.orientation || -90 === window.orientation) {
+      document.body.scrollTop = 0;
+    }
+  }, false);
+
+})();
