592b85847830010b4000369f29628823c1f3c0fe
Shindeok Kang <heavymery@gmail.com>
2014-07-25 01:52:34 +0900

    Update content template

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,7 +18,7 @@
 
   app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     
-    $routeProvider.when('/', { templateUrl: 'views/top.html' });
+    $routeProvider.when('/', { templateUrl: 'views/home.html' });
     $routeProvider.when('/profile', { templateUrl: 'views/profile.html' });
     $routeProvider.when('/portfolio', { templateUrl: 'views/portfolio.html' });
     $routeProvider.when('/contact', { templateUrl: 'views/contact.html' });
